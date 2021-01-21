f2c7d27ac5b6653468af5e9b83063522ae356961
Dan Büschlen <danbueschlen@gmail.com>
2021-01-21 21:10:56 +0100

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a29f289a9a46d4d5a0da";
+/******/ 	var hotCurrentHash = "a9aca69ce464bc10d2aa";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
